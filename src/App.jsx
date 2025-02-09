16aa4d10ab387a87fbced91db229a8e3cfecf743
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-02-09 11:21:57 +0530

    Wallet Connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import {WagmiProvider} from 'wagmi';
-import { config } from './utils/config';
+import {config} from "./utils/config.js";
 import {QueryClient,QueryClientProvider} from '@tanstack/react-query';
-import LandingPage from './pages/LandingPage';
-
+// import LandingPage from './pages/LandingPage';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WalletConnect from './pages/WalletConnect';
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <LandingPage/>
+        {/* <Router> */}
+          {/* <Routes> */}
+            {/* <Route path='/' element={<WalletConnect/>}/> */}
+            {/* <Route path='/page' element={<LandingPage/>}/> */}
+          {/* </Routes> */}
+        {/* </Router> */}
+        <WalletConnect/>
       </QueryClientProvider>
     </WagmiProvider>
   )
--- src/pages/WalletConnect.jsx
+++ src/pages/WalletConnect.jsx
@@ -1,13 +1,20 @@
-import {useConnect} from 'wagmi';
+import {useConnect,useDisconnect} from 'wagmi';
 const WalletConnect = () => {
 
     const {connectors,connect} = useConnect();
+    console.log(connectors);
+    const {disconnect} = useDisconnect();
 
-  return connectors.map((connector) => {
-    <button key={connector.uid} onClick={()=>connect({connector})}>
-        {connector.name}
+  return (
+    <>
+   {connectors.map((connector) => {
+    <button key={connector?.uid} onClick={()=>connect({connector})}>
+        {connector?.name}
     </button>
-  })
+  })}
+  <button onClick={()=>disconnect()}>Disconnect</button>
+  </>
+)
 }
 
 export default WalletConnect
